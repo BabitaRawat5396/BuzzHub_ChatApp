41dc803537acaa8ab72b046cb5e4f65ea6f89d41
Babita Rawat <singbabita12@gmail.com>
2023-08-29 09:31:52 +0530

    controllers profile

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getProfile } = require("../controllers/profile");
+const { getProfile } = require("../controllers/Profile");
 const router = express.Router();
 
 router.post("/getProfiles",getProfile);
